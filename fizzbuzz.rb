a9462212feb711a49d3b943c75d80a1eb6f66e91
Brad Hambrice <hambrice@uab.edu>
2017-12-04 21:52:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz (number)
-  if number % 3 && number % 5 
+  if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
-  elsif number % 3 
+  elsif number % 3 = 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
